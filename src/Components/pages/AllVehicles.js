0439cfe79b8d9a46d9c5d44411a51a45b91ec2c0
MerioGenesis <mariopif20@hotmail.com>
2025-11-15 17:07:21 +0000

    Update AllVehicles.js

--- src/Components/pages/AllVehicles.js
+++ src/Components/pages/AllVehicles.js
@@ -1,18 +1,14 @@
 import { useState, useEffect } from 'react';
 import API from '../api/API.js';
 import Table from '../UI/Table.js';
-import Panel from '../UI/Panel.js';
-import ObjectTable from '../UI/ObjectTable.js';
 
 
 
 export default function AllVehicles() {
     // Initialization ----------------------------
-    const loggedinUserID = 25;
     const endpoint = `/vcharter/vehicles/`;
 
     // State -------------------------------------
-    const [user, setUser] = useState(null);
     const [vehicles, setVehicles] = useState([]);
     const [loadingMessage, setLoadingMessage] = useState('Loading records...');
 
